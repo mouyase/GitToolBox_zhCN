41e138c16295353a730e0ddef4a2ff6f84368731
某亚瑟 <i@yojigen.tech>
2024-02-18 15:17:51 +0800

    feat: update ca

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env sh
 
-unzip gittoolbox-500.1.9_222-signed.zip
+version=500.1.10_222
+jarVersion=500.1.10+222
 
-cp zh_
+zipName=gittoolbox-"$version"-signed.zip
+jarName=gittoolbox-"$jarVersion".jar
+targetName=gittoolbox-"$version".zip
+
+rm -rf gittoolbox
+
+wget https://plugins.jetbrains.com/files/7499/487998/"$zipName" -O gittoolbox-"$version"-signed.zip
+
+unzip "$zipName"
+
+jar uf gittoolbox/lib/"$jarName" zh/*
+
+#unzip "$jarName"
+
+#jar -C origin/ xf gittoolbox/lib/"$jarName"
+
+#cp -rf zh/* origin/*
+#
+#cd origin
+#
+#jar xf "../$jarName" -C origin/
+#
+#cd ..
+#
+#cp -f "$jarName" gittoolbox/lib/"$jarName"
+#
+zip -r "$targetName" gittoolbox/*
