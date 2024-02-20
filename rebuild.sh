197aaef67a0094cfb80026147f2487f1524ed8f1
某亚瑟 <i@yojigen.tech>
2024-02-20 10:18:18 +0800

    fix: 修改文件下载脚本

--- rebuild.sh
+++ rebuild.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
-version=500.1.10_222
-jarVersion=500.1.10+222
+file=$(curl "https://plugins.jetbrains.com/api/plugins/7499/updates?channel=&size=8" | jq -r '.[0] | .file')
 
+version=$(echo $file | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+_[0-9]\+')
+version_jar=$(echo $version | tr '_' '+')
+ 
 zipName=gittoolbox-"$version"-signed.zip
-jarName=gittoolbox-"$jarVersion".jar
+jarName=gittoolbox-"$version_jar".jar
 targetName=gittoolbox-"$version".zip
 
 rm -rf gittoolbox
 
-wget https://plugins.jetbrains.com/files/7499/487998/"$zipName" -O gittoolbox-"$version"-signed.zip
+wget "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=490653" -O gittoolbox-"$version"-signed.zip
 
 unzip "$zipName"
 
--- update.sh
+++ update.sh
@@ -5,16 +5,13 @@ file=$(curl "https://plugins.jetbrains.com/api/plugins/7499/updates?channel=&siz
 version=$(echo $file | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+_[0-9]\+')
 version_jar=$(echo $version | tr '_' '+')
  
-echo $version
-echo $version_jar
-
 zipName=gittoolbox-"$version"-signed.zip
 jarName=gittoolbox-"$version_jar".jar
 targetName=gittoolbox-"$version".zip
 
 rm -rf gittoolbox
 
-wget https://plugins.jetbrains.com/files/7499/487998/"$zipName" -O gittoolbox-"$version"-signed.zip
+wget "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=490653" -O gittoolbox-"$version"-signed.zip
 
 unzip "$zipName"
 
