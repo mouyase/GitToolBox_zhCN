3b2ce53b14640008cb380ce071a86feddc71938f
某亚瑟 <i@yojigen.tech>
2024-02-20 10:45:36 +0800

    feat: 打包脚本更新

--- rebuild.sh
+++ rebuild.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-file=$(curl "https://plugins.jetbrains.com/api/plugins/7499/updates?channel=&size=8" | jq -r '.[0] | .file')
+json_file_name=version.json
+curl "https://plugins.jetbrains.com/api/plugins/7499/updates?channel=&size=8" -o $json_file_name
 
-version=$(echo $file | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+_[0-9]\+')
-version_jar=$(echo $version | tr '_' '+')
+file_id=$(cat $json_file_name | jq -r '.[0] | .id')
+version_jar=$(cat $json_file_name | jq -r '.[0] | .version')
+version=$(echo $version_jar | tr '+' '_')
  
 zipName=gittoolbox-"$version"-signed.zip
 jarName=gittoolbox-"$version_jar".jar
 targetName=gittoolbox-"$version".zip
 
-rm -rf gittoolbox
-
-wget "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=490653" -O gittoolbox-"$version"-signed.zip
+wget "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=$file_id" -O gittoolbox-"$version"-signed.zip
 
 unzip "$zipName"
 
@@ -22,3 +22,9 @@ cp -rf zh zielu
 jar uf gittoolbox/lib/"$jarName" zielu/*
 
 zip -r "$targetName" gittoolbox/*
+
+rm -rf gittoolbox
+rm -rf zielu
+rm -rf META-INF
+rm -rf $json_file_name
+rm -rf $targetName
